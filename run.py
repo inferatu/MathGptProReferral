3363a19410330908d3f2ea5418dac5a93603be2b
roka <maximkalosha@gmail.com>
2024-02-08 14:18:07 +0300

    "Improved input validation in run.py script
    Refactored to include better input validation in the script. Instead of simply asking for inputs, the code now contains loop structures that only break if valid inputs are received. Added a 'ValueError' exception for number of registrations input to handle non-integer inputs and a conditional check to make sure the input is not less than or equal to zero. Also optimized the input process for showing the registration process, ensuring that user can only input 'y' or 'n', prompting them to choose a valid option if anything else is entered.

--- run.py
+++ run.py
@@ -1,15 +1,29 @@
 from autoreferral import AutoReferral
 
+referral_link = str(input("Ссылка на ваш реферальный код: "))
 
-referall_link = str(input("Ссылка на ваш реферальный код: "))
-number_of_regestrations = int(input("Сколько добавить монеток? (1 аккаунт = 5 монеток): "))
+while True:
+    try:
+        number_of_registrations = int(input("Сколько добавить монеток? (1 аккаунт = 5 монеток): "))
+        if number_of_registrations <= 0:
+            print(f"Алё, я не могу зарегать {number_of_registrations} акков")
+        else:
+            break
+    except ValueError:
+        print("Вводи число, не буковки")
 
-if input("Показывать процесс? y/n: ").lower() == 'y':
-    show_process = True
-else:
-    show_process = False
+show_process = False
+while True:
+    user_input = input("Показывать процесс? y/n: ").lower()
+    if user_input == 'n':
+        break
+    elif user_input == 'y':
+        show_process = True
+        break
+    else:
+        print("Выбери y или n (y = да, n = нет)")
 
-referal = AutoReferral(referral_link=referall_link, number_of_registrations=number_of_regestrations,
+referal = AutoReferral(referral_link=referral_link, number_of_registrations=number_of_registrations,
                        show_process=show_process)
 
 referal.start_registration()
